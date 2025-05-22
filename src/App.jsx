d3b879cd1cd2a596772634d473aad314673b1bc1
mart2164 <mart2164@purdue.edu>
2025-05-22 17:44:32 -0400

    Inital Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-pink-500 to-yellow-500">
+      <h1 className="text-5xl font-bold text-white shadow-lg">
+        🎉 Tailwind is working!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
+
